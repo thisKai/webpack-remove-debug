e6b1490438e8d19b9eb92477b41c1e3a3713c70d
Kai Patrick <thiskaiguy@gmail.com>
2018-09-04 15:49:54 +0100

    Take extra whitespace into account

--- debug-stripper.js
+++ debug-stripper.js
@@ -1,4 +1,4 @@
-const stripDebugRequire = source => source.replace( /.*?require\(\s*[\'"]debug[\'"]\s*\).*/g, '\n' );
-const stripDebugCall = source => source.replace( /.*debug\(.*?\).*/g, '\n' );
+const stripDebugRequire = source => source.replace( /.*require\s*\(\s*['"]debug['"]\s*\).*/g, '\n' );
+const stripDebugCall = source => source.replace( /.*debug\s*\(.*?\).*/g, '\n' );
 
 module.exports = source => stripDebugRequire( stripDebugCall( source ) );
--- debug-stripper.test.js
+++ debug-stripper.test.js
@@ -9,6 +9,12 @@ test('Remove require("debug")(...)', t => {
         t.equal(debugStripper(input), expected);
     }
     {
+        const input = 'const debug = require ( "debug" ) ( "namespace" ) ;';
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    {
         const input = "const debug=require('debug')('namespace');";
         const expected = '\n';
 
@@ -20,6 +26,12 @@ test('Remove require("debug")(...)', t => {
 test('Remove calls to a debug instance named "debug"', t => {
     {
         const input = 'debug("message");';
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    {
+        const input = 'debug ( "message" ) ;';
         const expected = '\n';
 
         t.equal(debugStripper(input), expected);
