088e40136554efd5cad88386da5cc581a417cd89
Kai Patrick <thiskaiguy@gmail.com>
2018-09-04 10:02:09 +0100

    Create tests for removing debug imports and calls

---
+++ debug-stripper.test.js
@@ -0,0 +1,34 @@
+const test = require('tape');
+const debugStripper = require('./debug-stripper');
+
+test('Remove require("debug")(...)', t => {
+    {
+        const input = 'const debug=require("debug")("namespace");';
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    {
+        const input = "const debug=require('debug')('namespace');";
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    t.end();
+});
+
+test('Remove calls to a debug instance named "debug"', t => {
+    {
+        const input = 'debug("message");';
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    {
+        const input = "debug('message');";
+        const expected = '\n';
+
+        t.equal(debugStripper(input), expected);
+    }
+    t.end();
+});
