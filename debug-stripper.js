ed79550206ee2ef9430f0d9077bc693182ed2af8
Kai Patrick <thiskaiguy@gmail.com>
2018-09-05 10:00:26 +0100

    Return transformations so far when no more identifiers are found

--- debug-stripper.js
+++ debug-stripper.js
@@ -5,26 +5,40 @@ const stripDebugCall = source => source.replace( /.*debug\s*\(.*?\).*/g, '\n' );
 
 const debugImportPattern = /\s*import\s+([a-zA-Z_\$][a-zA-Z_\$0-9]*)\s+from\s+[\'"]debug[\'"]\s*;?/g;
 
-const getImportedDebugFactoryNames = source =>
-    getAllMatches(debugImportPattern, source)
+const getFirstCaptureGroups = (pattern, source) =>
+    getAllMatches(pattern, source)
         .map(matches => matches[1]);
+
+const getImportedDebugFactoryNames = source =>
+    getFirstCaptureGroups(debugImportPattern, source);
 
 const stripDebugImport = source => {
     const importNames = getImportedDebugFactoryNames(source);
 
+    if(!importNames.length){
+        return source;
+    }
+
     const importsRemoved = source.replace(debugImportPattern, '');
+
     const instancePattern = new RegExp(
         `\\s*(?:var|let|const)\\s+([a-zA-Z_\\$][a-zA-Z_\\$0-9]*)\\s*=\\s*(?:${importNames.join('|')})\\s*\\(.*\\)\\s*;?`,
         'g',
     );
+
+    const instanceNames = getFirstCaptureGroups(instancePattern, importsRemoved);
+
+    if(!instanceNames.length){
+        return importsRemoved;
+    }
+
     const instancesRemoved = importsRemoved.replace(instancePattern, '');
-    const instanceNames = getAllMatches(instancePattern, importsRemoved)
-        .map(matches => matches[1]);
 
     const callsPattern = new RegExp(
         `\\s*(${instanceNames.join('|')})\\s*\\(.*\\)\\s*;?`,
         'g',
     );
+
     const callsRemoved = instancesRemoved.replace(callsPattern, '');
 
     const enabledChecksPattern = new RegExp(
